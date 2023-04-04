60007b223a424d48914028f6e2fb7953cb2a584f
SebasPico24 <picoafanador@gmail.com>
2023-04-03 22:13:35 -0500

    Clase usuarios y clase ventana

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import GUI.ventanaLogin;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ventanaLogin ventana = new ventanaLogin();
+        ventana.setVisible(true);
     }
 }
